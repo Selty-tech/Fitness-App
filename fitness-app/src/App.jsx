b0ed87ddadfb113669744e34779c4b735e4afe1b
Selty-tech <sergichitadze24@gmail.com>
2026-06-16 18:00:48 +0200

    Removing comments

--- fitness-app/src/App.jsx
+++ fitness-app/src/App.jsx
@@ -1,9 +1,9 @@
-// import { useState } from 'react';
+
 import { useAuth} from './context/AuthContext';
 import './App.css'
 import Header from './components/Header';
 import UserInfo from './components/UserInfo';
-// import user from './data/user'
+
 import Login from './components/Login'
 
 
@@ -11,29 +11,9 @@ function App() {
 
   const { isLoggedIn} = useAuth();
 
-  // const [isLoggedIn, setIsLoggedIn] = useState(() => {
-  //   const savedStatus = sessionStorage.getItem('isLoggedIn');
-  //   return savedStatus === 'true'
-  // });
-
-  // const handleLogin = () => {
-  //   sessionStorage.setItem('isLoggedIn', 'true');
-  //   setIsLoggedIn(true);
-  // };
-
-
- 
-
-
   return (
     <div>
     <Header />
-     {/* {isLoggedIn ? (
-      <UserInfo user={user} />
-    ):  (
-      <Login onLogin={handleLogin} />
-    )} */}
-
     {isLoggedIn ? <UserInfo /> : <Login />}
 
   </div>
