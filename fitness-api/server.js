0f54444eb3f0eec038cce5eb8a8b3646498677d2
Selty-tech <sergichitadze24@gmail.com>
2026-08-04 21:23:55 +0200

    Implement backend login validation

--- fitness-api/server.js
+++ fitness-api/server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 const PORT = 3000;
 
+
 const user = {
   username: "test",
   password: "1234",
@@ -25,10 +26,20 @@ app.get("/", (request, response) => {
 
 
 app.post("/auth/login", (request, response) => {
+  const { username, password } = request.body; 
   console.log(request.body);
-
-  response.json(user);
+  if (
+    username === user.username &&
+    password === user.password
+  ) {
+    response.json(user);
+  } else {
+    response.status(401).json({
+      message: "Invalid username or password",
+    });
+  }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
