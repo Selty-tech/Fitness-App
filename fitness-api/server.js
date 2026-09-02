d2bc5243c237464502ae11900dd7f3c402a65b20
Selty-tech <sergichitadze24@gmail.com>
2026-09-02 22:27:53 +0200

    Add backend user registration

--- fitness-api/server.js
+++ fitness-api/server.js
@@ -43,6 +43,63 @@ app.post("/auth/login", async (request, response) => {
     response.json(safeUser);
   });
 
+  app.post('/auth/register', async (request, response) => {
+    const {
+      username,
+      password,
+      fullname,
+      sex,
+      birthdate,
+      weight,
+      height,
+      activityLevel,
+    } = request.body;
+
+    const getUser = db.prepare(`
+    SELECT * FROM users WHERE username = ?
+    `);
+
+    const user = getUser.get(username);
+
+    if (user) {
+      return response.status(409).json({
+        message: 'User with this username already exists.',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertUser = db.prepare(`
+        INSERT  INTO users(
+        username,
+        password,
+        fullname,
+        sex,
+        birthdate,
+        weight,
+        height,
+        activityLevel
+        )
+        VALUES(?,?,?,?,?,?,?,?)
+        `);
+
+    insertUser.run(
+      username,
+      hashedPassword,
+      fullname,
+      sex,
+      birthdate,
+      weight,
+      height,
+      activityLevel,
+    );
+    
+    const newUser = getUser.get(username);
+
+    const { password: _, ...safeUser } = newUser;
+    response.json(safeUser);
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
